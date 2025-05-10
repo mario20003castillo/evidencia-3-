f783ac8d772ca82c407f0e93c4dbb07202c69ac7
mario20003castillo <146145585+mario20003castillo@users.noreply.github.com>
2025-05-09 22:05:24 -0600

    Create joke.app.cs

---
+++ joke.app.cs
@@ -0,0 +1,12 @@
+﻿namespace menus.Models
+{
+    public class Joke
+    {
+        public string Category { get; set; }
+        public string Type { get; set; } 
+        public string Language { get; set; }
+        public string Content { get; set; }
+        public string Setup { get; set; }
+        public string Delivery { get; set; }
+    }
+}
